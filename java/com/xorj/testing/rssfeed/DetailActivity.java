79a37f8a22d2a1716135c22b4c74ec13b9b571f2
sfleuhku <brndnmxwll055@gmail.com>
2015-04-03 11:16:22 -0700

    trying

--- java/com/xorj/testing/rssfeed/DetailActivity.java
+++ java/com/xorj/testing/rssfeed/DetailActivity.java
@@ -14,10 +14,10 @@ public class DetailActivity extends Activity {
 
         // Need to check if Activity has been switched to landscape mode
         // If yes, finished and go back to the start Activity
-        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+        /*if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
             finish();
             return;
-        }
+        }*/
         setContentView(R.layout.activity_detail);
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
